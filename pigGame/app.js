95dfb4c2b753f49689278c1e3c98e5418f6fee0e
MCClarke <mcarlotclarke@gmail.com>
2020-05-08 16:46:38 -0400

    add images, set up dice roll and next player

--- pigGame/app.js
+++ pigGame/app.js
@@ -2,7 +2,7 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he whishes. Each result gets added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
@@ -15,8 +15,51 @@ NEXT level of rules:
 
 */
 
+// Declare needed variables
 let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
+
+// Hide dice image at start of the game, set all values to 0
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+
+const nextPlayer = () => {
+  // Switch player and set roundScore = 0
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  // Set to 0 also in the UI
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  // Toggle active player
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  //Remove the dice from the UI
+  document.getElementById('dice-1').style.display = 'none';
+};
+
+// Set up dice click event
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  // Roll random number
+  const dice1 = Math.floor(Math.random() * 6) + 1;
+
+  // Display dice image with rolled number
+  document.getElementById('dice-1').style.display = 'block';
+
+  document.getElementById('dice-1').src = `dice-${dice1}.png`;
+
+  if (dice1 !== 1) {
+    // Add score
+    roundScore += dice1;
+    document.querySelector(`#score-${activePlayer}`).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
+});
