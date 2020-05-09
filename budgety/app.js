31b574e0a351cf2721662e84c6260b96687c6d47
MCClarke <mcarlotclarke@gmail.com>
2020-05-09 16:22:14 -0400

    Get input field data

--- budgety/app.js
+++ budgety/app.js
@@ -2,13 +2,38 @@
 const budgetController = (() => {})();
 
 // UI controller
-const uiController = (() => {})();
+const uiController = (() => {
+  const domStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn',
+  };
+
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(domStrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value,
+      };
+    },
+
+    getDomStrings: () => {
+      return domStrings;
+    },
+  };
+})();
 
 // App controller -> central place to decide and control what happens in each event and delegate tasks to other controllers
 // Takes two parameters - Then we pass the UI and budget controllers as arguments to the IIFE
 const controller = ((budgetCtrl, uiCtrl) => {
+  const domStringsUICtrl = uiCtrl.getDomStrings();
+
   const controllerAddItem = () => {
     // 1. Get the input field data
+    const input = uiCtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate budget
@@ -16,7 +41,7 @@ const controller = ((budgetCtrl, uiCtrl) => {
   };
 
   document
-    .querySelector('.add__btn')
+    .querySelector(domStringsUICtrl.inputButton)
     .addEventListener('click', controllerAddItem);
   document.addEventListener('keypress', (event) => {
     if (event.keyCode === 13 || event.which === 13) {
