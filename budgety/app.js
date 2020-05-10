c9956ec72e7cda01b4e554d92b22e7fa30c42336
MCClarke <mcarlotclarke@gmail.com>
2020-05-09 20:25:25 -0400

    Add init function to start app

--- budgety/app.js
+++ budgety/app.js
@@ -28,24 +28,34 @@ const uiController = (() => {
 // App controller -> central place to decide and control what happens in each event and delegate tasks to other controllers
 // Takes two parameters - Then we pass the UI and budget controllers as arguments to the IIFE
 const controller = ((budgetCtrl, uiCtrl) => {
-  const domStringsUICtrl = uiCtrl.getDomStrings();
-
   const controllerAddItem = () => {
     // 1. Get the input field data
     const input = uiCtrl.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate budget
     // 5. Display the budget on the UI
   };
 
-  document
-    .querySelector(domStringsUICtrl.inputButton)
-    .addEventListener('click', controllerAddItem);
-  document.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      controllerAddItem();
-    }
-  });
+  const setUpEventListeners = () => {
+    const domStringsUICtrl = uiCtrl.getDomStrings();
+
+    document
+      .querySelector(domStringsUICtrl.inputButton)
+      .addEventListener('click', controllerAddItem);
+    document.addEventListener('keypress', (event) => {
+      if (event.keyCode === 13 || event.which === 13) {
+        controllerAddItem();
+      }
+    });
+  };
+
+  return {
+    init: () => {
+      setUpEventListeners();
+    },
+  };
 })(budgetController, uiController);
+
+// To execute all the code when app starts like setUpEventListeners
+controller.init();
