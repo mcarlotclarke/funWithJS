6313fddebf4812e0b404882f85ed6146186bb421
MCClarke <mcarlotclarke@gmail.com>
2020-05-10 17:27:16 -0400

    Add income, expenses and new item to budget controller

--- budgety/app.js
+++ budgety/app.js
@@ -1,5 +1,58 @@
 // Budget controller
-const budgetController = (() => {})();
+const budgetController = (() => {
+  // Create constructor functions & data structure
+  const Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      totalExp: 0,
+      totalInc: 0,
+    },
+  };
+
+  return {
+    addItem: (type, desc, val) => {
+      let newItem, iD;
+
+      // Create new id
+      if (data.allItems[type].length > 0) {
+        iD = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        iD = 0;
+      }
+
+      // Create new item based on 'exp' or 'inc' type
+      if (type === 'inc') {
+        newItem = new Income(iD, desc, val);
+      } else if (type === 'exp') {
+        newItem = new Expense(iD, desc, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+      // Return new element
+      return newItem;
+    },
+
+    testing() {
+      console.log(data);
+    },
+  };
+})();
 
 // UI controller
 const uiController = (() => {
@@ -29,9 +82,11 @@ const uiController = (() => {
 // Takes two parameters - Then we pass the UI and budget controllers as arguments to the IIFE
 const controller = ((budgetCtrl, uiCtrl) => {
   const controllerAddItem = () => {
+    let input, newItem;
     // 1. Get the input field data
-    const input = uiCtrl.getInput();
+    input = uiCtrl.getInput();
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     // 4. Calculate budget
     // 5. Display the budget on the UI
