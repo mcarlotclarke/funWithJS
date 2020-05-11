20642f06869bdd29a948213538df9c50263f1357
MCClarke <mcarlotclarke@gmail.com>
2020-05-11 12:07:33 -0400

    Update budget: budget controller - calculate budget, get budget

--- budgety/app.js
+++ budgety/app.js
@@ -19,9 +19,20 @@ const budgetController = (() => {
       inc: [],
     },
     totals: {
-      totalExp: 0,
-      totalInc: 0,
+      exp: 0,
+      inc: 0,
     },
+    budget: 0,
+    percentage: -1,
+  };
+
+  const calculateTotal = (type) => {
+    let sum = 0;
+
+    data.allItems[type].forEach((element) => {
+      sum += element.value;
+    });
+    data.totals[type] = sum;
   };
 
   return {
@@ -46,6 +57,31 @@ const budgetController = (() => {
       data.allItems[type].push(newItem);
       // Return new element
       return newItem;
+    },
+
+    calculateBudget: () => {
+      // Calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // Calculate the budget -> income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // Calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
+
+    getBudget: () => {
+      return {
+        budget: data.budget,
+        inc: data.totals.inc,
+        exp: data.totals.exp,
+        percentage: data.percentage,
+      };
     },
 
     // to test our app temporarily
@@ -121,10 +157,17 @@ const uiController = (() => {
 // App controller -> central place to decide and control what happens in each event and delegate tasks to other controllers
 // Takes two parameters - Then we pass the UI and budget controllers as arguments to the IIFE
 const controller = ((budgetCtrl, uiCtrl) => {
-  const updateBudget = () => {
-    // 1. Calculate budget
-    // 2. Return the budget
-    // 3. Display the budget on the UI
+  const setUpEventListeners = () => {
+    const domStringsUICtrl = uiCtrl.getDomStrings();
+
+    document
+      .querySelector(domStringsUICtrl.inputButton)
+      .addEventListener('click', controllerAddItem);
+    document.addEventListener('keypress', (event) => {
+      if (event.keyCode === 13 || event.which === 13) {
+        controllerAddItem();
+      }
+    });
   };
 
   const controllerAddItem = () => {
@@ -150,17 +193,15 @@ const controller = ((budgetCtrl, uiCtrl) => {
     }
   };
 
-  const setUpEventListeners = () => {
-    const domStringsUICtrl = uiCtrl.getDomStrings();
+  const updateBudget = () => {
+    // 1. Calculate budget
+    budgetCtrl.calculateBudget();
 
-    document
-      .querySelector(domStringsUICtrl.inputButton)
-      .addEventListener('click', controllerAddItem);
-    document.addEventListener('keypress', (event) => {
-      if (event.keyCode === 13 || event.which === 13) {
-        controllerAddItem();
-      }
-    });
+    // 2. Return the budget
+    const budget = budgetCtrl.getBudget();
+
+    // 3. Display the budget on the UI
+    console.log(budget);
   };
 
   return {
