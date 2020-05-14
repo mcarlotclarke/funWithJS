a4979e7dc88035a0bf8c427924114fc9ba133877
MCClarke <mcarlotclarke@gmail.com>
2020-05-14 18:40:47 -0400

    add additional set up files and webpack config

---
+++ forkify/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'inline-source-map', // to fix: DevTools failed to load SourceMap: Could not load content for webpack:///node_modules/sockjs-client/dist/sockjs.js.map: HTTP error: status code 404, net::ERR_UNKNOWN_URL_SCHEME
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+};
