507dc914bd45c344680dc9d11644574ba57b610a
MCClarke <mcarlotclarke@gmail.com>
2020-05-15 08:52:56 -0400

    add and build Search controller

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,7 +1,35 @@
-// Global app controller
+/*
+In Search.js const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+In Recipe.js const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+Documentation URL http://forkify-api.herokuapp.com/
+ */
 
-import num from './test';
+import Search from './models/Search';
 
-const x = 23;
+/* Global state of the app
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipes
+*/
+const state = {};
 
-console.log(`I imported ${num} again to see the change. ADD variable ${x}`);
+const controlSearch = async () => {
+  // 1. Get the query from the view
+  const query = 'pizza';
+
+  if (query) {
+    // 2. New serch object and add to state
+    state.search = new Search(query);
+    // 3. Prepare UI for results
+    // 4. Search for recipes
+    await state.search.getResults();
+    // 5. Render results in UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', (event) => {
+  event.preventDefault();
+  controlSearch();
+});
