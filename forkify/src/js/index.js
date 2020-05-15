4ca7d31acb8c461f3600afef7d91cbd627544fdf
MCClarke <mcarlotclarke@gmail.com>
2020-05-15 10:15:03 -0400

    add searchView and initial render results

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -5,6 +5,8 @@ Documentation URL http://forkify-api.herokuapp.com/
  */
 
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /* Global state of the app
 - Search object
@@ -16,20 +18,24 @@ const state = {};
 
 const controlSearch = async () => {
   // 1. Get the query from the view
-  const query = 'pizza';
+  const query = searchView.getInput();
 
   if (query) {
     // 2. New serch object and add to state
     state.search = new Search(query);
+
     // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
     // 4. Search for recipes
     await state.search.getResults();
     // 5. Render results in UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector('.search').addEventListener('submit', (event) => {
+elements.searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
   controlSearch();
 });
